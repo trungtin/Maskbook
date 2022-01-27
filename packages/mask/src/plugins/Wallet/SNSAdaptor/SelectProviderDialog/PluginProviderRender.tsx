97dd88d32e72316ce585d7043587acbdedf8b978
guanbinrui <guanbinrui@dimension.im>
2022-01-27 11:03:06 +0800

    Revert "chore: replace network icon style (#5519)"
    This reverts commit ae09ff4b627125b86c2f17f1302bc83b004cc460.

--- packages/mask/src/plugins/Wallet/SNSAdaptor/SelectProviderDialog/PluginProviderRender.tsx
+++ packages/mask/src/plugins/Wallet/SNSAdaptor/SelectProviderDialog/PluginProviderRender.tsx
@@ -52,7 +52,6 @@ const useStyles = makeStyles()((theme) => ({
         height: 14,
         background: '#fff',
         borderRadius: '50%',
-        border: `1px solid ${theme.palette.background.default}`,
     },
     alert: {
         fontSize: 12,
